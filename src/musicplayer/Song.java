07e416d1ab67b3862eb5da2e2e02c0b65e1dc241
konoha1216 <zhengrag@usc.edu>
2021-04-13 22:19:48 +0800

    music player - class Song and SongTest

---
+++ src/musicplayer/Song.java
@@ -0,0 +1,61 @@
+package musicplayer;
+
+import java.util.Objects;
+
+/**
+ * 歌曲类
+ */
+public class Song {
+    private String id;
+    private String name;
+    private String singer;
+
+    public Song(String id, String name, String singer) {
+        this.id = id;
+        this.name = name;
+        this.singer = singer;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return Objects.equals(id, song.id) && Objects.equals(name, song.name) && Objects.equals(singer, song.singer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, singer);
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", singer='" + singer + '\'' +
+                '}';
+    }
+}
